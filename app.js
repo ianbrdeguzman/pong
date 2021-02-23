2555c45e8141d2398bdcd1fa12c3fd24f45e62cc
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-02-22 20:23:56 -0500

    added end game sounds

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const startBtn = document.querySelector('#start')
 const pausedScreen = document.querySelector('.paused-screen');
 const winnerScreen = document.querySelector('.winner-screen');
 const loserScreen = document.querySelector('.loser-screen');
+const numberOfRounds = document.querySelector('input[type=number]');
 
 // create canvas
 const canvas = document.querySelector('canvas');
@@ -10,8 +11,8 @@ const ctx = canvas.getContext('2d');
 
 // responsive canvas
 const resizeCanvas = () => {
-    canvas.width = 600;
-    canvas.height = 400;
+    canvas.width = 900;
+    canvas.height = 600;
 };
 
 // resize event listener
@@ -25,31 +26,40 @@ resizeCanvas();
 
 // global variable
 let animationId;
+let goingUp = false;
+let goingDown = false;
+let rounds = 3;
 
 // load audio
 const hitAudio = new Audio();
 const wallAudio = new Audio();
 const enemyScoreAudio = new Audio();
 const playerScoreAudio = new Audio();
+const winnerAudio = new Audio();
+const loserAudio = new Audio();
 
 // assign audio source
 hitAudio.src = './sounds/hit.mp3';
 wallAudio.src = './sounds/wall.mp3';
 enemyScoreAudio.src = './sounds/comScore.mp3';
 playerScoreAudio.src = './sounds/userScore.mp3';
+winnerAudio.src = `./sounds/winner.wav`;
+loserAudio.src = `./sounds/loser.wav`;
+
 
 // ball class
 class Ball {
+    // constructor function
     constructor() {
         this.x = canvas.width / 2;
         this.y = canvas.height / 2;
         this.speed = 3;
-        this.dx = this.speed + (Math.random() - 0.5);
-        this.dy = this.speed + (Math.random() - 0.5);
+        this.dx = (Math.random() > 0.5) ? this.speed : -this.speed;
+        this.dy = (Math.random() > 0.5) ? this.speed : -this.speed;
         this.radius = 10;
         this.color = 'orange';
     }
-
+    // draw function
     draw() {
         ctx.strokeStyle = this.color;
         ctx.fillStyle = this.color;
@@ -58,22 +68,27 @@ class Ball {
         ctx.fill();
         ctx.stroke();
     }
-
+    // update function
     update() {
         this.draw();
+
+        // keep ball moving
         this.y += this.dy;
         this.x += this.dx;
 
+        // check if ball in between 0 and canvas.height
         if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            // play bounce audio
             wallAudio.play();
+            // change y direction
             this.dy = -this.dy;
         }
-
     }
 }
 
 // paddle class
 class Paddle {
+    // constructor function
     constructor(x, y) {
         this.width = 10;
         this.height = 50;
@@ -81,17 +96,17 @@ class Paddle {
         this.y = y;
         this.score = 0;
     }
-
+    // draw function
     draw() {
         ctx.fillStyle = 'white';
         ctx.beginPath();
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-
-    update() {
+    // paddle ai
+    ai() {
         this.draw();
 
-        this.y += ((ball.y - (this.y + this.height / 2))) * 0.1;
+        this.y += ((ball.y - (this.y + this.height / 2))) * 0.15;
 
         if (this.y + this.height > canvas.height) {
             this.y = canvas.height - this.height;
@@ -100,7 +115,20 @@ class Paddle {
         if (this.y < 0) {
             this.y = 0;
         }
-        
+    }
+    // player movement
+    movement() {
+        if (goingUp) this.up();
+
+        if (goingDown) this.down();
+    }
+    // move paddle up if paddle is greater than 0
+    up() {
+        if (this.y > 0) this.y -= 5;
+    }
+    // move paddle down if paddle is less than canvas height
+    down() {
+        if (this.y + this.height < canvas.height) this.y += 5;
     }
 }
 
@@ -136,29 +164,6 @@ const drawGame = () => {
     enemy.draw();
 };
 
-const scoreControl = () => {
-
-    // check if score is >= 3
-    if (player.score >= 3 || enemy.score >= 3) {
-
-        // if player score is greater
-        if (player.score > enemy.score) {
-
-            // show winner screen
-            winnerScreen.classList.toggle('retry');
-
-        // if enemy score is greater
-        } else {
-
-            // show loser screen
-            loserScreen.classList.toggle('retry');
-        }
-        
-        // cancel animation
-        cancelAnimationFrame(animationId);
-    }
-};
-
 const collisionControl = (ball, paddle) => {
 
     // assign paddle positions
@@ -185,17 +190,14 @@ const gameLoop = () => {
     // draw game
     drawGame();
 
-    // control score every frame
-    scoreControl();
-    
     // update ball every frame
     ball.update();
 
     // draw player every frame
-    player.draw();
+    player.movement();
     
     // update enemy every frame
-    enemy.update();
+    enemy.ai();
     
     // check if paddle is player or enemy
     let paddle = (ball.x + ball.radius < canvas.width / 2) ? player : enemy;
@@ -211,48 +213,73 @@ const gameLoop = () => {
         let angle = (Math.PI / 4) * point;
         let direction = (ball.x + ball.radius < canvas.width / 2) ? 1 : -1;
 
-        ball.dx = direction * (ball.speed * 2) * Math.cos(angle);
-        ball.dy = (ball.speed * 2) * Math.sin(angle);
+        ball.dx = direction * Math.cos(angle) * (ball.speed * 2.2) ;
+        ball.dy = Math.sin(angle) * (ball.speed * 2.2);
     }
 
     // check if ball is out of right canvas
     if (ball.x + ball.radius > canvas.width) {
         
-        // show paused screen
-        pausedScreen.classList.toggle('ready');
+        if (player.score < rounds - 1) {
+            // show paused screen
+            pausedScreen.classList.toggle('ready');
 
-        // increase score
-        player.score++;
+            // increase score
+            player.score++;
 
-        // reset ball location
-        ball.x = canvas.width / 2;
-        ball.y = canvas.height / 2;
+            // reset ball location
+            ball.x = canvas.width / 2;
+            ball.y = canvas.height / 2;
 
         // play audio
-        playerScoreAudio.play();
+            playerScoreAudio.play();
         
         // pause animation frame
-        cancelAnimationFrame(animationId);
+            cancelAnimationFrame(animationId);
+        } else {
+            player.score++;
+            // draw score
+            drawScore();
+            // play winner audio
+            winnerAudio.play();
+            // show winner screen
+            winnerScreen.classList.toggle('retry');
+            // cancel gameLoop
+            cancelAnimationFrame(animationId);
+        }
+
     }
 
     // check if ball is out of left canvas
     if(ball.x - ball.radius < 0) {
         
-        // show paused screen
-        pausedScreen.classList.toggle('ready');
+        if (enemy.score < rounds - 1) {
+            // show paused screen
+            pausedScreen.classList.toggle('ready');
 
-        // increase score
-        enemy.score++;
+            // increase score
+            enemy.score++;
 
-        // reset ball location
-        ball.x = canvas.width / 2;
-        ball.y = canvas.height / 2;
+            // reset ball location
+            ball.x = canvas.width / 2;
+            ball.y = canvas.height / 2;
 
-        // play audio
-        enemyScoreAudio.play();
+            // play audio
+            enemyScoreAudio.play();
         
-        // pause animation frame
-        cancelAnimationFrame(animationId);
+            // pause animation frame
+            cancelAnimationFrame(animationId);
+        } else {
+            enemy.score++;
+            // draw score
+            drawScore();
+            // play loser audio
+            loserAudio.play();
+            // show loser screen
+            loserScreen.classList.toggle('retry');
+            // cancel gameLoop
+            cancelAnimationFrame(animationId);
+        }
     }
 
     // draw score
@@ -279,6 +306,8 @@ window.addEventListener('click', (e) => {
         // hide menu
         document.querySelector('.menu-screen').classList.toggle('hide-menu');
 
+        // change rounds value
+        rounds = numberOfRounds.value;
         // start game
         gameLoop();
     }
@@ -306,14 +335,29 @@ window.addEventListener('keydown', (e) => {
         
         // code for arrow up key
         case 38:
-            if(player.y > 0) player.y -= 50;
+            goingUp = true;
             break;
 
         // code for arrow down key
         case 40:
-            if(player.y + player.height < canvas.height) player.y += 50;
+            goingDown = true;
             break;
     }
 });
 
+window.addEventListener('keyup', (e) => {
+
+    switch (e.keyCode) {
+        
+        // code for arrow up key
+        case 38:
+            goingUp = false;
+            break;
+
+        // code for arrow down key
+        case 40:
+            goingDown = false;
+            break;
+    }
+});
 
